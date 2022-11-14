8878d5c04d6a50a5f3d055f6fcd3716f7756809a
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-11-14 14:39:05 +0000

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,9 +1,32 @@
+# Constants
 
-pi = 3.14159265358979323846
+pi = 3.14159265358979323846  # the ratio of a circle's circumference to its diameter
 mu0 = 4 * pi * 1.00000000055 * pow(10, -7)  # H/m
 epsilon0 = 8.8541878128 * pow(10, -12)  # F / m-1
 euler = 2.718281828459045235360287471352
 c = 299792458  # metres per second
+
+
+class BoltzmannConstant():
+    def __init__(self):
+        self.init = self
+
+    @staticmethod
+    def J_per_K():
+        return 1.380649 * pow(10, -23)
+
+    @staticmethod
+    def eV_per_K():
+        return 8.617333262 * pow(10, -5)
+
+    @staticmethod
+    def Hz_per_K():
+        return 2.083661912 * pow(10, 10)
+
+    @staticmethod
+    def erg_per_K():
+        return 1.380649 * pow(10, -16)
+
 
 
 @staticmethod
